130f59093fa78517642964398b90a2628886212a
CudoXY <gelo103097@yahoo.com>
2017-07-15 14:28:37 +0800

    Fixed material viewing

--- src/main/java/library/controllers/MaterialController.java
+++ src/main/java/library/controllers/MaterialController.java
@@ -1,5 +1,7 @@
 package library.controllers;
 
+import library.domain.Borrow;
+import library.services.BorrowService;
 import library.services.MaterialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,29 +9,68 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MaterialController
 {
-
+    private BorrowService borrowService;
     private MaterialService materialService;
+
+    @Autowired
+    public void setBorrowService(BorrowService borrowService) {
+        this.borrowService = borrowService;
+    }
 
     @Autowired
     public void setMaterialService(MaterialService materialService) {
         this.materialService = materialService;
     }
 
+//	@RequestMapping("/catalog")
+//	public String viewCatalog(
+//			@RequestParam(value = "c", required = false) Integer categoryId,
+//			@RequestParam(value = "search", required = false) String search,
+//			@RequestParam(value = "by", required = false) String by,
+//			Model model)
+//	{
+//		model.addAttribute("message", "Catalog poh");
+//		model.addAttribute("parameter", categoryId + " " + search + " " + by);
+//		System.out.println("viewCatalog");
+//		return "user/material_view";
+//	}
+
     @RequestMapping(value = "/catalog", method = RequestMethod.GET)
-    public String list(Model model){
-        model.addAttribute("materialList", materialService.getMaterialList());
-        System.out.println("Returning products:");
+    public String get(
+            @RequestParam(value = "category_id", required = false, defaultValue = "-1") int categoryId, Model model){
+
+        if (categoryId  == -1)
+            model.addAttribute("materialList", materialService.getMaterialList());
+        else
+            model.addAttribute("materialList", materialService.getMaterialListByCategory(categoryId));
+
         return "user/material_list";
     }
 
-    @RequestMapping("catalog/{categoryId}")
-    public String listByCategory(@PathVariable Integer categoryId, Model model){
-        model.addAttribute("materialList", materialService.getMaterialListByCategory(categoryId));
-        return "user/material_list";
+
+    @RequestMapping("/catalog/view")
+    public String view(
+            @RequestParam(value = "id") String materialId,
+            Model model)
+    {
+        System.out.println("before viewMaterial");
+        model.addAttribute("material", materialService.getMaterialById(materialId));
+        System.out.println("viewMaterial");
+        return "user/material_view";
+    }
+
+
+    @RequestMapping(value = "/view/borrow", method = RequestMethod.POST)
+    public String borrow(Borrow borrow)
+    {
+        borrowService.borrowMaterial(borrow);
+        System.out.println("borrowMaterial");
+        return "redirect:/view?id=" + borrow.getId();
     }
 //
 //    @RequestMapping("product/edit/{id}")
