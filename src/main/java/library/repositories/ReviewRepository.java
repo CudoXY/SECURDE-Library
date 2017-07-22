70d7b046d91e2ba47f09721e3843d20a349297e8
CudoXY <gelo103097@yahoo.com>
2017-07-23 00:54:30 +0800

    Implemented review (no restriction yet)

--- src/main/java/library/repositories/ReviewRepository.java
+++ src/main/java/library/repositories/ReviewRepository.java
@@ -2,6 +2,13 @@ package library.repositories;
 
 import library.domain.Review;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ReviewRepository extends CrudRepository<Review, Integer>{
+
+	@Transactional
+	Iterable<Review> findAllByMaterial_Id(String materialId);
+
+	@Transactional
+	Iterable<Review> findAllByMaterial_IdOrderByDateReviewedDesc(String materialId);
 }
