9564716647d39d2a922d85aaa723904088b12f4a
CudoXY <gelo103097@yahoo.com>
2017-08-11 02:32:17 +0800

    Fixed cannot cast User to CurrentUser

--- src/main/java/library/controllers/CurrentUserControllerAdvice.java
+++ src/main/java/library/controllers/CurrentUserControllerAdvice.java
@@ -4,6 +4,7 @@ import library.domain.CurrentUser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
@@ -14,9 +15,9 @@ public class CurrentUserControllerAdvice
 	private static final Logger LOGGER = LoggerFactory.getLogger(CurrentUserControllerAdvice.class);
 
 	@ModelAttribute("currentUser")
-	public CurrentUser getCurrentUser(Authentication authentication)
+	public User getCurrentUser(Authentication authentication)
 	{
-		return (authentication == null) ? null : (CurrentUser) authentication.getPrincipal();
+		return (authentication == null) ? null : (User) authentication.getPrincipal();
 	}
 
 
