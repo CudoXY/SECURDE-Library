e2955f1b74dbaad284919e59377fffc5ffe020b9
CudoXY <gelo103097@yahoo.com>
2017-08-10 19:47:29 +0800

    Implemented auto login; Fixed secret question not loading on registration error

--- src/main/java/library/services/currentuser/CurrentUserServiceImpl.java
+++ src/main/java/library/services/currentuser/CurrentUserServiceImpl.java
@@ -4,6 +4,12 @@ import library.domain.CurrentUser;
 import library.domain.Role;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +17,43 @@ public class CurrentUserServiceImpl implements CurrentUserService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CurrentUserDetailsService.class);
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private CurrentUserDetailsService userDetailsService;
+
     @Override
     public boolean canAccessUser(CurrentUser currentUser, int userId) {
         LOGGER.debug("Checking if user={} has access to user={}", currentUser, userId);
         return currentUser != null
                 && (currentUser.getRole() == Role.ROLE_ADMIN || currentUser.getId() == userId);
+    }
+
+    @Override
+    public void autologin(String username, String password) {
+
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+
+        System.out.println("userDetails = " + userDetails.getUsername());
+        try
+        {
+            authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+        }
+        catch (AuthenticationException e)
+        {
+            System.out.println("authenticate failed");
+        }
+
+        if (usernamePasswordAuthenticationToken.isAuthenticated()) {
+            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            System.out.println(String.format("Auto login %s successfully!", username));
+            return;
+        }
+
+        System.out.println("Failed to login hehe");
     }
 
 }
