d11b5839e1b7f787f39041bec7ff2e8a742b4ca8
CudoXY <gelo103097@yahoo.com>
2017-08-14 02:27:46 +0800

    Updated findOne to findOneById

--- src/main/java/library/repositories/UserRepository.java
+++ src/main/java/library/repositories/UserRepository.java
@@ -2,10 +2,15 @@ package library.repositories;
 
 import library.domain.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Integer>
 {
+	@Transactional
+	User findOneById(int id);
+
+	@Transactional
 	User findOneByEmail(String email);
 }
